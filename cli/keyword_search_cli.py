1a3e86d2250b95ec3698a6e1f12a56be8c9a0b36
shokhjakhonabdunabiev <20shoxjaxon02@gmail.com>
2026-08-22 22:40:59 +0200

    inverted index impl

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,24 +1,32 @@
 import argparse
 
-from lib.keyword_search import search_command
+from lib.keyword_search import build_command, search_command
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    search_parser = subparsers.add_parser("search", help="Search movies using keywords")
+    subparsers.add_parser("build", help="Build the inverted index")
+
+    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
     args = parser.parse_args()
 
     match args.command:
+        case "build":
+            print("Building inverted index...")
+            build_command()
+            print("Inverted index built successfully.")
         case "search":
-            print(f"Searching for: {args.query}")
+            print("Searching for:", args.query)
             results = search_command(args.query)
             for i, res in enumerate(results, 1):
-                print(f"{i}. {res['title']}")
+                print(f"{i}. ({res['id']}) {res['title']}")
         case _:
             parser.print_help()
+
 
 if __name__ == "__main__":
     main()
