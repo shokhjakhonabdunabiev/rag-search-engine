03d141a1e9786adaeb6069635d90b7ae60ab9570
shokhjakhonabdunabiev <20shoxjaxon02@gmail.com>
2026-08-23 20:22:15 +0200

    length normalization

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -9,7 +9,7 @@ from lib.keyword_search import (
     tf_command,
     tfidf_command,
 )
-from lib.search_utils import BM25_K1
+from lib.search_utils import BM25_B, BM25_K1
 
 
 def main() -> None:
@@ -53,6 +53,9 @@ def main() -> None:
     bm25_tf_parser.add_argument(
         "k1", type=float, nargs="?", default=BM25_K1, help="Tunable BM25 K1 parameter"
     )
+    bm25_tf_parser.add_argument(
+        "b", type=float, nargs="?", default=BM25_B, help="Tunable BM25 b parameter"
+    )
 
     args = parser.parse_args()
 
@@ -81,7 +84,7 @@ def main() -> None:
             bm25idf = bm25_idf_command(args.term)
             print(f"BM25 IDF score of '{args.term}': {bm25idf:.2f}")
         case "bm25tf":
-            bm25tf = bm25_tf_command(args.doc_id, args.term, args.k1)
+            bm25tf = bm25_tf_command(args.doc_id, args.term, args.k1, args.b)
             print(
                 f"BM25 TF score of '{args.term}' in document '{args.doc_id}': {bm25tf:.2f}"
             )
